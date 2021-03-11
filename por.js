1dd1ba9a4723626356e05db9c7b022bb538fd9c7
habeebah <omowumimuse@gmail.com>
2021-03-11 17:39:03 -0500

    enter

---
+++ por.js
@@ -0,0 +1,8 @@
+// var headingEl = document.getElementsByClassName(p2);
+// console.log(headingEl);
+// var headingEl = document.body.innerHTML = ("hello.YOU");
+// console.log(headingEl);
+var strong = document.querySelectorAll(".strong");
+for (i=0; i<strong;i++) {
+    strong[0].innerHTML = "One";
+}
